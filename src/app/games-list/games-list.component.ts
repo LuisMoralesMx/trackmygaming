0ec4934fd2cf474162e2f671ad078211030fa1a9
Luis Morales <lmorales@mty.ipointsystems.com>
2018-04-27 15:50:20 -0500

    Added new model to handle game properties.

--- src/app/games-list/games-list.component.ts
+++ src/app/games-list/games-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game.model';
 
 @Component({
   selector: 'app-games-list',
@@ -9,7 +10,7 @@ export class GamesListComponent implements OnInit {
 
   // Declaring component variables
   platform = 'PS4';
-  gamesList = Array<{id: number, title: string, platform: string, releasedate: string}>();
+  gamesList = Array<Game>();
 
   constructor() { }
 
@@ -20,10 +21,10 @@ export class GamesListComponent implements OnInit {
   getGameList() {
     
    this.gamesList = [
-      {id: 1, title: 'Last Of Us', platform: 'PS4', releasedate: 'July 29, 2014'},
-      {id: 2, title: 'God of War', platform: 'PS4', releasedate: 'April 20, 2018'},
-      {id: 3, title: 'Horizon Zero Dawn', platform: 'PS4', releasedate: 'February 28, 2017'},
-      {id: 4, title: 'Uncharted 4: A Thief\'s End', platform: 'PS4', releasedate: 'May 10, 2016'}      
+      {id: 1, title: 'Last Of Us', platform: 'PS4', releasedate: 'July 29, 2014', status: 'Completed'},
+      {id: 2, title: 'God of War', platform: 'PS4', releasedate: 'April 20, 2018', status: 'In Progress'},
+      {id: 3, title: 'Horizon Zero Dawn', platform: 'PS4', releasedate: 'February 28, 2017', status: 'Completed'},
+      {id: 4, title: 'Uncharted 4: A Thief\'s End', platform: 'PS4', releasedate: 'May 10, 2016', status: 'Completed'}      
     ];
   }
 
---
+++ src/app/models/game.model.ts
@@ -0,0 +1,9 @@
+export class Game {
+    constructor(
+        public id: number,
+        public title: string,
+        public platform: string,
+        public releasedate: string,
+        public status: string,
+    ) { }
+}
