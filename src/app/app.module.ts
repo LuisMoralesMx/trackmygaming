05b7efb82fc060f9ad855510bc97fd61bb0df3f9
Luis Morales <lmorales@mty.ipointsystems.com>
2018-04-27 10:26:41 -0500

    Initial version.

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { GamesListComponent } from './games-list/games-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GamesListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/games-list/games-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games-list',
+  templateUrl: './games-list.component.html',
+  styleUrls: ['./games-list.component.scss']
+})
+export class GamesListComponent implements OnInit {
+
+  // Declaring component variables
+  platform = 'PS4';
+  gamesList = Array<{id: number, title: string, platform: string, releasedate: string}>();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.getGameList();
+  }
+
+  getGameList() {
+    
+   this.gamesList = [
+      {id: 1, title: 'Last Of Us', platform: 'PS4', releasedate: 'July 29, 2014'},
+      {id: 2, title: 'God of War', platform: 'PS4', releasedate: 'April 20, 2018'},
+      {id: 3, title: 'Horizon Zero Dawn', platform: 'PS4', releasedate: 'February 28, 2017'},
+      {id: 4, title: 'Uncharted 4: A Thief\'s End', platform: 'PS4', releasedate: 'May 10, 2016'}      
+    ];
+
+  }
+}
